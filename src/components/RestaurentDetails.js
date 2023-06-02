555bce85ef86604e11efe720ea61eca94f86fc59
Laxman <laxmankundaragi2000@gmail.com>
2023-06-02 17:52:14 +0530

    This is second commit.....message

--- src/components/RestaurentDetails.js
+++ src/components/RestaurentDetails.js
@@ -6,7 +6,7 @@ class RestaurentDetails extends Component {
             <div> &nbsp;
                  <div>
       <p>
-       iLink company having more than 2000 employee leading company...
+       iLink company having more than 2000+ employee leading company...
            </p>
       <p>
         Top ten company here this <strong>i-Link</strong> also one of them... <a href="https://th.bing.com/th?id=OIP.d-juZ0uQ2WLNL7g7kHpsvgHaE8&w=306&h=204&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2'">Logo of i-link</a>.
